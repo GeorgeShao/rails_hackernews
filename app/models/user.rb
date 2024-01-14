a3883ee2e78f2962eb0f5a0235d49717e41a8183
George Shao <GeorgeShao123@gmail.com>
2024-01-14 11:00:34 -0500

    setup Post<>User relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_07_172650) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_14_155456) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "link"
     t.boolean "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
